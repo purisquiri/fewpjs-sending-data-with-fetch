bcad5883c73d07b2f2830a60d769697e7eb51b38
purisquiri <mau.dorigo@outlook.es>
2020-11-19 14:19:38 -0500

    commit tree 7b86214f8546dc7d69605e1a974964d7d8112131

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+// document.getElementById('submit').addEventListener("click", function () {
+//     let formData = {
+//         //call the event.dogName....
+//         dogName: ,
+//         dogBreed: "Poodle"
+//     };
+
+//     let configObj = {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "applicaton/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify(formData)
+//     };
+
+//     fetch('http://localhost:3000/dogs', configObj)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (object) {
+//             console.log(object);
+//         })
+// }
+// );
+
+
